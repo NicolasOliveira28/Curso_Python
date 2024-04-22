bfd8d1e13a775549f60f087c4c57e3e4514bff03
NicolasOliveira28 <nicolasoffshore@gmail.com>
2024-04-22 15:20:12 -0300

    adição do meu progresso em aulas.

---
+++ exercício2.py
@@ -0,0 +1,9 @@
+nome = 'Nicolas Oliveira'
+altura = 1.71
+peso = 67
+imc = peso / (altura * altura)
+
+print(nome)
+print('tem'), print(altura), print('de altura.')
+print('pesa'), print(peso), print('quilos')
+print('e o seu IMC é:'), print(imc)
