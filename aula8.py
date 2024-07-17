530b1f7acf77ccf814525cd0ce4b40a5bf5a1c8e
NicolasOliveira28 <nicolasoffshore@gmail.com>
2024-07-17 18:32:49 -0300

    ...

--- aula8.py
+++ aula8.py
@@ -16,7 +16,7 @@ print('Divisão inteira', divisao_inteira)
 exponenciacao = 2 ** 10
 print('Exponenciação', exponenciacao)
 
-modulo = 55 % 2  # resto da divisão
+modulo = 55 % 2  # restos da divisão
 print('Módulo', modulo)
 
 print(10 % 8 == 0)
