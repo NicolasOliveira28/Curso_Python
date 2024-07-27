fde05b0d68b54a6ca794f37afcfbe1c6b35593a0
NicolasOliveira28 <nicolasoffshore@gmail.com>
2024-07-27 17:36:12 -0300

    ...

--- aula18.py
+++ aula18.py
@@ -3,7 +3,7 @@
 # and - Todas as condições precisam ser
 # verdadeiras.
 # Se qualquer valor for considerado falso,
-# a expressão inteira será avaliada naquele valor
+# a expressão inteira será avaliada naquele valor.
 # São considerados falsy (que vc já viu)
 # 0 0.0 '' False
 # Também existe o tipo None que é
