ea67edd096ce24d659e9f1aa6be32566ab74f76d
NicolasOliveira28 <nicolasoffshore@gmail.com>
2024-08-14 20:54:24 -0300

    ...,

--- aula25.py
+++ aula25.py
@@ -12,7 +12,7 @@ try:
     print('FLOAT:', numero_float)
     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
 except:
-    print('Isso não é um número.')
+    print('Isso não é um número!')
 
 # if numero_str.isdigit():
 #     numero_float = float(numero_str)
