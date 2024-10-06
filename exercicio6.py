34b6c594e20037b0a86cb902a97fb7625a583057
NicolasOliveira28 <nicolasoffshore@gmail.com>
2024-10-05 21:27:50 -0300

    adição de exercicio concluido

---
+++ exercicio6.py
@@ -0,0 +1,13 @@
+entrada = input('Digite um Número: ')
+
+if entrada.isdigit():
+    entrada_int = int(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'Ímpar'
+    
+    if par_impar:
+        par_impar_texto = 'Par'
+
+    print(f'O número {entrada_int} é {par_impar_texto}')
+else:
+    print('Você não digitou um número inteiro!')
