9a0b85df2349c8ba8dd7a70aa68d7c21c18b8595
NicolasOliveira28 <nicolasoffshore@gmail.com>
2024-10-06 21:35:35 -0300

    ...

--- exercicio6.py
+++ exercicio6.py
@@ -3,10 +3,10 @@ entrada = input('Digite um Número: ')
 if entrada.isdigit():
     entrada_int = int(entrada)
     par_impar = entrada_int % 2 == 0
-    par_impar_texto = 'Ímpar'
+    par_impar_texto = 'Ímpar!'
     
     if par_impar:
-        par_impar_texto = 'Par'
+        par_impar_texto = 'Par!'
 
     print(f'O número {entrada_int} é {par_impar_texto}')
 else:
