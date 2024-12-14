dfbbc0b115a411e6f52ecccc2ae8832fe77636f9
NicolasOliveira28 <nicolasoffshore@gmail.com>
2024-12-13 23:19:14 -0300

    ..

--- aula26.py
+++ aula26.py
@@ -7,5 +7,5 @@ velocidade = 61  # velocidade atual do carro
 local_carro = 100  # local em que o carro está na estrada
 
 RADAR_1 = 60  # velocidade máxima do radar 1
-LOCAL_1 = 100  # local onde o radar 1 está
+LOCAL_1 = 100  # locais onde o radar 1 está
 RADAR_RANGE = 1  # A distância onde o radar pegar
